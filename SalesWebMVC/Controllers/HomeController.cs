4a2891c92f609a393a905af6f37d4d712bdabe26
JhonnyPrado <jmatheus_prado@hotmail.com>
2024-01-14 16:42:15 -0700

    Criado Departments Model e DbContext

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -17,7 +18,7 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App Testando um projeto CRUD";
 
             return View();
         }
---
+++ SalesWebMVC/Data/SalesWebMvcContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Data
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {   
+        }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
